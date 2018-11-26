0c91317264433a744275e0ca2b6f548435cb9d19
h15039 <h15039@h15039d.h3c.huawei-3com.com>
2018-11-26 16:10:06 +0800

    test

--- demo/src/main/java/com/h3c/services/dao/impl/UserDao.java
+++ demo/src/main/java/com/h3c/services/dao/impl/UserDao.java
@@ -1,10 +1,39 @@
 package com.h3c.services.dao.impl;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.h3c.base.models.User;
 import com.h3c.services.dao.interfaces.IUserDao;
 
 @Repository("UserDao")
 public class UserDao implements IUserDao{
+	@Autowired
+	@Qualifier("sessionFactory")
+	protected SessionFactory sessionFactory;
+	
+	@Autowired
+	@Qualifier("hibernateTemplate")
+	protected HibernateTemplate hibernateTemplate;
 
+	public void save(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void delete(String userName) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
 }
--- demo/src/main/java/com/h3c/services/dao/interfaces/IUserDao.java
+++ demo/src/main/java/com/h3c/services/dao/interfaces/IUserDao.java
@@ -1,5 +1,23 @@
 package com.h3c.services.dao.interfaces;
 
-public interface IUserDao {
+import com.h3c.base.models.User;
 
+public interface IUserDao {
+	/**
+	 * Ôö
+	 * @param user
+	 */
+	public void save(User user);
+	
+	/**
+	 * ¸Ä
+	 * @param user
+	 */
+	public void update(User user);
+	
+	/**
+	 * É¾
+	 * @param userName
+	 */
+	public void delete(String userName);
 }
