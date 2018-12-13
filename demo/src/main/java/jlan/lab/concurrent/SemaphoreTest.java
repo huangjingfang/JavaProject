42c8e6e147431f895bb00f8728b384c7581f2c46
h15039 <h15039@h15039d.h3c.huawei-3com.com>
2018-12-13 16:42:02 +0800

    semaphore test

--- demo/src/main/java/jlan/lab/concurrent/SemaphoreTest.java
+++ demo/src/main/java/jlan/lab/concurrent/SemaphoreTest.java
@@ -3,15 +3,85 @@ package jlan.lab.concurrent;
 import java.util.concurrent.Semaphore;
 
 public class SemaphoreTest implements Runnable {
-
+	Object lock = new Object();
+	static Semaphore startSemaphore = new Semaphore(3);
+	static Semaphore stopSemaphore = new Semaphore(0);
+	ThreadLocal<Semaphore> threadLocal = new ThreadLocal<Semaphore>();
+	
 	public void run() {
 		// TODO Auto-generated method stub
+		synchronized (lock) {
+			for(int i=0;i<10;i++) {
+				try {
+					startSemaphore.acquire();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println(Thread.currentThread().getName()+":\t"+i);
+				try {
+					lock.wait();
+					System.out.println("notified");
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Semaphore semaphore = new Semaphore(3);
+		Semaphore semaphore = new Semaphore(1);
+		System.out.println(semaphore.availablePermits());
+		semaphore.release(10);
+		System.out.println(semaphore.availablePermits());
+		semaphore.drainPermits();
+		System.out.println(semaphore.availablePermits());
+	/*	try {
+			semaphore.acquire(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		System.out.println(semaphore.availablePermits());
+		
+		SemaphoreTest test = new SemaphoreTest();
+		for(int i =0;i<3;i++) {
+			new Thread(test).start();
+		}
+		
+		new Thread(test.new Manager()).start();
 	}
 
+	
+	class Manager implements Runnable{
+
+		public void run() {
+			// TODO Auto-generated method stub
+			synchronized (lock) {
+				for(int i=0;i<10;i++) {
+					while(startSemaphore.availablePermits()!=0) {
+						try {
+							Thread.sleep(100);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					System.out.println("Manager:\t"+i);
+					startSemaphore.release(3);
+					lock.notifyAll();
+					try {
+						lock.wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		
+	}
 }
