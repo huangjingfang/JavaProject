e512836453ab58f40a58d5cb363b0e103cfd2f32
h15039 <h15039@h15039d.h3c.huawei-3com.com>
2018-12-14 20:00:17 +0800

    controller

--- web/web/src/main/java/com/h3c/web/controllers/HelloController.java
+++ web/web/src/main/java/com/h3c/web/controllers/HelloController.java
@@ -1,5 +1,8 @@
 package com.h3c.web.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
 
 @Controller
 @RequestMapping(value="/hello")
@@ -18,5 +23,16 @@ public class HelloController {
 	public String sayHello(HttpServletRequest request,HttpServletResponse response,Model model) {
 		a++;
 		return "hello SpringMVC:"+a;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="json",produces="application/json; charset=UTF-8",method= {RequestMethod.GET})
+	public String getJson() {
+		List<String> list = new ArrayList<String>();
+		list.add("apple");
+		list.add("orange");
+		list.add("nut");
+		
+		return new Gson().toJson(list);
 	}
 }
