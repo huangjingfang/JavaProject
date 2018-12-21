5a5fac81beea5cf06dd3d86ba6243376e7bb03a5
h15039 <h15039@h15039d.h3c.huawei-3com.com>
2018-12-21 18:04:38 +0800

    java 8

---
+++ demo/src/main/java/jlan/java8/functionref/FunctionReferenceTest.java
@@ -0,0 +1,18 @@
+package jlan.java8.functionref;
+
+/**
+ *  Suppiler 接口
+ * @author h15039
+ *
+ */
+public class FunctionReferenceTest {
+	
+	
+	public static void main(String[] args) {
+			
+	}
+	
+	public void function() {
+		
+	}
+}
---
+++ demo/src/main/java/jlan/java8/stream/StreamTest.java
@@ -0,0 +1,20 @@
+package jlan.java8.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list = new ArrayList<>();
+		for(int i=0;i<10000;i++) {
+			list.add(String.valueOf(i));
+		}
+		Stream<String> stream = list.stream();
+		stream.forEach((x)->System.out.println(x.substring(1)));
+		
+	}
+
+}
