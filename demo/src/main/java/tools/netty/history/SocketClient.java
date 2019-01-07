3fe2caca3702b3f3198c938c081921662f62f655
h15039 <h15039@h15039d.h3c.huawei-3com.com>
2019-01-07 11:59:56 +0800

    socket

--- demo/src/main/java/tools/netty/history/SocketClient.java
+++ demo/src/main/java/tools/netty/history/SocketClient.java
@@ -11,16 +11,49 @@ import java.net.Socket;
 
 public class SocketClient {
 	public static void main(String[] args) throws IOException {
+		ioStream();
+//		readerAndWriter();
+	}
+
+	public static void ioStream() throws IOException {
+		// 要连接的服务端IP地址和端口
+	    String host = "127.0.0.1";
+	    int port = 55533;
+	    // 与服务端建立连接
+	    Socket socket = new Socket(host, port);
+	    // 建立连接后获得输出流
+	    OutputStream outputStream = socket.getOutputStream();
+	    String message = "你好  yiwangzhibujian";
+	    socket.getOutputStream().write(message.getBytes("UTF-8"));
+	    //通过shutdownOutput高速服务器已经发送完数据，后续只能接受数据
+	    socket.shutdownOutput();
+	    
+	    InputStream inputStream = socket.getInputStream();
+	    byte[] bytes = new byte[1024];
+	    int len;
+	    StringBuilder sb = new StringBuilder();
+	    while ((len = inputStream.read(bytes)) != -1) {
+	      //注意指定编码格式，发送方和接收方一定要统一，建议使用UTF-8
+	      sb.append(new String(bytes, 0, len,"UTF-8"));
+	    }
+	    System.out.println("get message from server: " + sb);
+	    
+	    inputStream.close();
+	    outputStream.close();
+	    socket.close();
+	}
+
+	public static void readerAndWriter() throws IOException {
 		// 要连接的服务端IP地址和端口
 		String host = "127.0.0.1";
 		int port = 55533;
 		// 与服务端建立连接
 		Socket socket = new Socket(host, port);
 		// 建立连接后获得输出流
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(),"utf-8"));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"));
 		String message = "你好  yiwangzhibujian";
 		writer.write(message);
-		
+
 		// 通过shutdownOutput高速服务器已经发送完数据，后续只能接受数据
 		socket.shutdownOutput();
 
@@ -39,7 +72,7 @@ public class SocketClient {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		reader.close();
 		writer.close();
 		socket.close();
--- demo/src/main/java/tools/netty/history/SocketServer.java
+++ demo/src/main/java/tools/netty/history/SocketServer.java
@@ -13,6 +13,39 @@ import java.net.Socket;
 public class SocketServer {
 
 	public static void main(String[] args) throws IOException {
+		ioStream();
+	}
+
+	public static void ioStream() throws IOException {
+		// 监听指定的端口
+		int port = 55533;
+		ServerSocket server = new ServerSocket(port);
+
+		// server将一直等待连接的到来
+		System.out.println("server将一直等待连接的到来");
+		Socket socket = server.accept();
+		// 建立好连接后，从socket中获取输入流，并建立缓冲区进行读取
+		InputStream inputStream = socket.getInputStream();
+		byte[] bytes = new byte[1024];
+		int len;
+		StringBuilder sb = new StringBuilder();
+		// 只有当客户端关闭它的输出流的时候，服务端才能取得结尾的-1
+		while ((len = inputStream.read(bytes)) != -1) {
+			// 注意指定编码格式，发送方和接收方一定要统一，建议使用UTF-8
+			sb.append(new String(bytes, 0, len, "UTF-8"));
+		}
+		System.out.println("get message from client: " + sb);
+
+		OutputStream outputStream = socket.getOutputStream();
+		outputStream.write("Hello Client,I get the message.".getBytes("UTF-8"));
+
+		inputStream.close();
+		outputStream.close();
+		socket.close();
+		server.close();
+	}
+
+	public static void readerAndwriter() throws IOException {
 		// 监听指定的端口
 		int port = 55533;
 		ServerSocket server = new ServerSocket(port);
@@ -30,9 +63,8 @@ public class SocketServer {
 			System.out.println("get message from client: " + sb);
 			sb.append(line);
 		}
-		
 
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(),"utf-8"));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"));
 		writer.write("Hello Client,I get the message.");
 
 		try {
@@ -46,5 +78,4 @@ public class SocketServer {
 		socket.close();
 		server.close();
 	}
-
 }
