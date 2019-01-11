07e35181b1d1ce3f8e1ba314ce4e4762bad37f45
h15039 <h15039@h15039d.h3c.huawei-3com.com>
2019-01-11 17:21:20 +0800

    int

--- demo/src/main/java/jlan/test/strange/IntegerEquals.java
+++ demo/src/main/java/jlan/test/strange/IntegerEquals.java
@@ -5,8 +5,9 @@ public class IntegerEquals {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Integer a = new Integer(100);
-		Integer b = new Integer(100);
-		System.out.println(a==b); //true
+//		Integer b = new Integer(100);
+		int b = 100;
+		System.out.println(a==b); //true,if b is Integer, a==b is false
 		
 		Integer c = new Integer(200);
 		Integer d = 200;
