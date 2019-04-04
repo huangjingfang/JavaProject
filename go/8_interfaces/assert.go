c528d742fe4208ad0efde0bd69a807b1b86922a1
huangjingfang <kolgan_hjf@163.com>
2019-04-04 16:38:38 +0800

    struct

--- go/8_interfaces/assert.go
+++ go/8_interfaces/assert.go
@@ -6,24 +6,45 @@ import (
 
 type inter interface{}
 
-func main(){
+func main() {
 	str := "assert"
 	bo := false
-	var any inter;
+	var any inter
 	any = str
 	assertString(&any)
 	assertBool(&any)
 
-	any = bo;
+	any = bo
 	assertString(&any)
 	assertBool(&any)
+
+	fmt.Println("**************************************************************")
+	assert(&any)
+	any = str
+	assert(&any)
+
 }
 
-func assertString(any *inter){
-	v,ok:= (*any).(string)
-	fmt.Println(v,ok)
+func assertString(any *inter) {
+	v, ok := (*any).(string)
+	fmt.Println(v, ok)
 }
-func assertBool(any *inter){
-	v,ok:= (*any).(bool)
-	fmt.Println(v,ok)
+func assertBool(any *inter) {
+	v, ok := (*any).(bool)
+	fmt.Println(v, ok)
+}
+func assert(any *inter) {
+	//interface.(type)只能在switch语句中
+	//v,ok := (*any).(type)
+	//fmt.Println()
+	switch (*any).(type) {
+	case string:
+		fmt.Println((*any).(string))
+	case int:
+		fmt.Println((*any).(int))
+	case bool:
+		fmt.Println((*any).(bool))
+	default:
+		fmt.Println((*any).(string))
+	}
 }
